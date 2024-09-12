550e5006259914b0bd7c51cb73f2c63f49fdb6a7
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-09-13 01:09:16 +0300

    mobile menu and links are created

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import MobileMenu from "./MobileMenu";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<div className="h-24 flex items-center justify-between">
+			<div className=" md:hidden xl:flex">
+				<Link href="/">
+					<Image
+						src={"/dk-high-resolution-logo-transparent.svg"}
+						alt="logo"
+						width={100}
+						height={100}
+					/>
+				</Link>
+			</div>
+			<div className="hidden md:flex w-[50%]">
+				<div className="flex space-x-2 items-center justify-center font-medium text-lg">
+					<Link
+						href={"/"}
+						className="flex space-x-2 items-center jutify-center"
+					>
+						<Image src="/home.png" alt="alt" width={16} height={16} />
+						<span>Home</span>
+					</Link>
+					<Link
+						href={"/friends"}
+						className="flex space-x-2 items-center jutify-center"
+					>
+						<Image src="/friends.png" alt="alt" width={16} height={16} />
+						<span>Friends</span>
+					</Link>
+					<Link
+						href={"/stories"}
+						className="flex space-x-2 items-center jutify-center"
+					>
+						<Image src="/stories.png" alt="alt" width={16} height={16} />
+						<span>Stories</span>
+					</Link>
+				</div>
+			</div>
+			<div className="w-[30%">
+				<MobileMenu />
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
